a8c78aed33dcb5d3023b2b21879c53889f5ff3f6
Bernard Bougeon <bernard.bougeon@gmail.com>
2016-02-21 18:24:51 +0100

    init

--- communities/static/com.js
+++ communities/static/com.js
@@ -1,12 +1,10 @@
 var comApp = angular.module('comApp', []);
 
-comApp.controller('CommunitiesListCtrl', function ($scope) {
-  $scope.communities = [
-    {'title': 'ddddddddd',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+
+comApp.controller('CommunitiesListCtrl', function ($scope, $http) {
+  $http.get('apiv1.0/communities').success(function(data) {
+    $scope.phones = data;
+  });
+
+  //$scope.orderProp = 'age';
 });
