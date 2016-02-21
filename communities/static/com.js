d7214217580a7043e9b83ca1b6b9b101cec9229e
Bernard Bougeon <bernard.bougeon@gmail.com>
2016-02-21 19:56:33 +0100

    init

--- communities/static/com.js
+++ communities/static/com.js
@@ -4,6 +4,7 @@ var comApp = angular.module('comApp', []);
 comApp.controller('CommunitiesListCtrl', function ($scope, $http) {
   $http.get('apiv1.0/communities').success(function(data) {
     $scope.communities = data;
+    alert(data);
   });
 
   //$scope.orderProp = 'age';
