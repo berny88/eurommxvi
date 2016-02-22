c814216fb34f4c54ad6aaa27ce4bfc2926992e08
Bernard Bougeon <bernard.bougeon@gmail.com>
2016-02-22 19:16:22 +0100

    init

--- communities/CommunityServices.py
+++ communities/CommunityServices.py
@@ -46,7 +46,7 @@ def communities():
 
 @communities_page.route('/apiv1.0/communities', methods=['GET'])
 def getAllComunnities():
-    logger.info("{}", jsonify({'communities': coms}))
+    logger.info(">>{}".format(jsonify({'communities': coms})))
     return (u"[\n"
             u"    {\n"
             u"        \"id\": 1,\n"
