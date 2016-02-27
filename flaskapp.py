a205ed96cfb34a9aa88ae6c409c9e9c74369c280
Bernard Bougeon <bernard.bougeon@gmail.com>
2016-02-27 10:04:58 +0100

    prepare send email

--- flaskapp.py
+++ flaskapp.py
@@ -57,10 +57,6 @@ def testmongo():
     tool = ToolManager()
     db = tool.getDb()
     logger.info(u'test mongo : db={}'.format(db))
-    result = db.properties.insert_one({'x': 1})
-    logger.info(u"insered_id".format(result.inserted_id));
-    doc = db.properties.find_one({'x': 1})
-    logger.info(u'test mongo : doc={}'.format(doc))
     tmp = tool.getProperty(u"test")
     logger.info(u'test mongo : tmp={}'.format(tmp))
     tool.saveProperty(u"test", "firstProperties")
