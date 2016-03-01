46291ea2a82f4ad1348f3d8b50a1fbf868440ffa
toto.squilaci <toto.squilaci>
2016-03-01 23:43:35 +0100

    fire !

--- communities/static/com.js
+++ communities/static/com.js
@@ -1,18 +1,52 @@
-var comApp = angular.module('comApp', []);
+var comApp = angular.module('comApp', ['ngTouch', 'ui.grid', 'ui.grid.edit']);
 
 
-comApp.controller('CommunitiesListCtrl', function ($scope, $http) {
-        $http.get('apiv1.0/communities').success(function(data) {
-        $scope.communities = data;
-        $scope.phones = [
-            {'name': 'Nexus S',
-             'snippet': 'Fast just got faster with Nexus S.'},
-            {'name': 'Motorola XOOM™ with Wi-Fi',
-             'snippet': 'The Next, Next Generation tablet.'},
-            {'name': 'MOTOROLA XOOM™',
-             'snippet': 'The Next, Next Generation tablet.'}
-          ];
-    });
+comApp.controller('CommunitiesListCtrl', ['$scope', '$http', '$q', function ($scope, $http, $q) {
 
-  //$scope.orderProp = 'age';
-});
+        // REMOTE DATAS :
+        $scope.gridOptions = {
+        };
+
+        $scope.gridOptions.columnDefs = [
+            {name:'id', displayName: 'Id Tech.', enableCellEdit: false, width: '20%' },
+            {name:'title', displayName: 'Titre (editable)', width: '40%'},
+            {name:'description', displayName: 'Description (editable)', width: '40%'}
+
+        ];
+
+        var canceler = $q.defer();
+
+        $scope.getCommunities = function() {
+            $http.get('apiv1.0/communities', {timeout: canceler.promise})
+            .success(function(data) {
+                //ng-repeat :
+                $scope.communities = data;
+                //ui-grid :
+                $scope.gridOptions.data = data.communities;
+            });
+        }
+        $scope.getCommunities();
+
+        $scope.$on('$destroy', function(){
+            canceler.resolve();  // Aborts the $http request if it isn't finished.
+        });
+
+        $scope.addData = function() {
+            var n = $scope.gridOptions.data.length + 1;
+            $scope.gridOptions.data.push({
+                "id": n,
+                "title": "Titre" + n,
+                "description": "Desc " + n
+
+            });
+        };
+
+        $scope.removeFirstRow = function() {
+           $scope.gridOptions.data.splice(0,1);
+        };
+
+        $scope.reset = function () {
+            $scope.getCommunities();
+        }
+
+}]);
