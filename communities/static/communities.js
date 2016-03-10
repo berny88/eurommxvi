9bbc3042fe98474f696a12f5140572f12cb2636c
toto.squilaci <toto.squilaci>
2016-03-10 22:02:48 +0100

    fire !

--- communities/static/communities.js
+++ communities/static/communities.js
@@ -5,18 +5,27 @@ euro2016App.controller('CommunitiesCtrl', ['$scope', '$routeParams', '$http', '$
         $scope.community = {};
         $scope.communityToDelete = {};
         $scope.communities = {};
+        hideAlerts();
 
         $scope.getCommunities = function() {
+            hideAlerts();
             $http.get('communities/apiv1.0/communities', {timeout: canceler.promise})
-            .success(function(data) {
+            .success(function(data, status, headers, config) {
                 $scope.communities = data;
+            })
+            .error(function(data, status, headers, config) {
+                showAlertError("Erreur lors de la récupération de la liste des communautés ; erreur HTTP : " + status);
             });
         }
 
         $scope.getCommunity = function() {
+            hideAlerts();
             $http.get('communities/apiv1.0/communities/' + $routeParams.com_id, {timeout: canceler.promise})
             .success(function(data, status, headers, config) {
                 $scope.community = data;
+            })
+            .error(function(data, status, headers, config) {
+                showAlertError("Erreur lors de la récupération de la communauté ; erreur HTTP : " + status);
             });
         }
 
@@ -27,19 +36,22 @@ euro2016App.controller('CommunitiesCtrl', ['$scope', '$routeParams', '$http', '$
 
         // launched when the "OK" button of the confirmation modal is pressed :
         $scope.confirm = function() {
+            hideAlerts();
             $http.delete('communities/apiv1.0/communities/' + $scope.communityToDelete.com_id, {timeout: canceler.promise})
             .success(function(data, status, headers, config) {
                 $scope.communities = data;
                 closeModal();
+                showAlertSuccess("Communauté [" + $scope.communityToDelete.title + "] supprimée avec succès !");
             })
             .error(function(data, status, headers, config) {
                 closeModal();
-                alert("Erreur :"+status)
+                showAlertError("Erreur lors de la suppression de la communauté ; erreur HTTP : " + status);
             });
         }
 
         // Aborts the $http request if it isn't finished.
         $scope.$on('$destroy', function(){
+            hideAlerts();
             canceler.resolve();
         });
 
