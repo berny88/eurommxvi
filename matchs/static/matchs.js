51cf9a1328c8a8fac7fc6e87f0988de5a4fa41c3
Bernard Bougeon <bernard.bougeon@gmail.com>
2016-03-28 18:24:46 +0200

    maj matchs

--- matchs/static/matchs.js
+++ matchs/static/matchs.js
@@ -1,4 +1,4 @@
-euro2016App.controller('matchsCtrl', ['$scope', '$http', '$q', function ($scope, $http, $q) {
+euro2016App.controller('matchsCtrl', ['$scope', '$http', '$q', '$timeout', '$window', function ($scope, $http, $q, $timeout, $window) {
 
         var canceler = $q.defer();
 
@@ -7,7 +7,26 @@ euro2016App.controller('matchsCtrl', ['$scope', '$http', '$q', function ($scope,
             .success(function(data) {
                 //ng-repeat :
                 $scope.matchs = data.matchs;
+                $scope.displaySaveButton = false;
+                if (isConnected($window)) {
+                    $scope.displaySaveButton = true;
+                }
             });
+        }
+
+        $scope.saveMatchs = function() {
+            $http.put('matchs/apiv1.0/matchs', {matchs: $scope.matchs, timeout: canceler.promise})
+            .success(function(data, status, headers, config) {
+                //showAlertSuccess("Paris sauvegardés !");
+                $.notify("Matchs sauvegardés !" , "success");
+            })
+            .error(function(data, status, headers, config) {
+                if (status==403){
+                    showAlertError("Même pas en rêve ! status=" + status+ " " + data);
+                } else {
+                    showAlertError("Erreur lors de la mise à jour des matchs ; erreur HTTP : " + status);
+                }
+            })
         }
 
         $scope.$on('$destroy', function(){
@@ -15,5 +34,8 @@ euro2016App.controller('matchsCtrl', ['$scope', '$http', '$q', function ($scope,
         });
 
         //$scope.getMatchs();
+        // to disable the input fields in the form
+
+
 
 }]);
