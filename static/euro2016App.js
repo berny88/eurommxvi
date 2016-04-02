6b083b9ad67624604fe38ca5b704a61d32458cb2
toto.squilaci <toto.squilaci>
2016-04-02 22:49:17 +0200

    Merge branch 'master' of ssh://euroxxxvi-typhontonus.rhcloud.com/~/git/euroxxxvi

--- static/euro2016App.js
+++ static/euro2016App.js
@@ -30,3 +30,22 @@ euro2016App.controller('topbarCtrl', function($scope, $window) {
         return isConnected($window);
     }
 });
+
+//Go to top
+jQuery(document).ready(function() {
+    var offset = 200;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.scroll-to-top').fadeIn(duration);
+        } else {
+            jQuery('.scroll-to-top').fadeOut(duration);
+        }
+    });
+
+    jQuery('.scroll-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
