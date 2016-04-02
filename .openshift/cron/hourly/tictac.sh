5d482d8cd47eae33dc64c580168fe7ef3d46c5b2
Bernard Bougeon <bernard.bougeon@gmail.com>
2016-04-02 12:15:40 +0200

    test cron

--- .openshift/cron/hourly/tictac.sh
+++ .openshift/cron/hourly/tictac.sh
@@ -2,4 +2,4 @@
 date >> ${OPENSHIFT_LOG_DIR}/tictac.log
 
 source $OPENSHIFT_HOMEDIR/python/virtenv/venv/bin/activate
-python ${OPENSHIFT_REPO_DIR}/pyt.py >> ${OPENSHIFT_LOG_DIR}/tictac.log
+python ${OPENSHIFT_REPO_DIR}/cron_hourly.py >> ${OPENSHIFT_LOG_DIR}/tictac.log
