9d7a2cc0f94b0fac63142aec47d45450bdf84662
toto.squilaci <toto.squilaci>
2016-04-23 18:09:17 +0200

    Merge branch 'master' of ssh://euroxxxvi-typhontonus.rhcloud.com/~/git/euroxxxvi

--- static/euro2016App.js
+++ static/euro2016App.js
@@ -20,34 +20,53 @@ euro2016App.controller('indexCtrl', function($scope, $window) {
         return isConnected($window);
     }
 
-    $("#DateCountdown").TimeCircles({
-        "animation": "smooth",
-        "bg_width": 1.2,
-        "fg_width": 0.1,
-        "circle_bg_color": "#60686F",
-        "time": {
-            "Days": {
-                "text": "Jours",
-                "color": "#FFCC66",
-                "show": true
-            },
-            "Hours": {
-                "text": "Heures",
-                "color": "#99CCFF",
-                "show": true
-            },
-            "Minutes": {
-                "text": "Minutes",
-                "color": "#BBFFBB",
-                "show": true
-            },
-            "Seconds": {
-                "text": "Secondes",
-                "color": "#FF9999",
-                "show": true
+    function js_yyyy_mm_dd_hh_mm_ss (myDate) {
+      year = "" + myDate.getFullYear();
+      month = "" + (myDate.getMonth() + 1); if (month.length == 1) { month = "0" + month; }
+      day = "" + myDate.getDate(); if (day.length == 1) { day = "0" + day; }
+      hour = "" + myDate.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+      minute = "" + myDate.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+      second = "" + myDate.getSeconds(); if (second.length == 1) { second = "0" + second; }
+      return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+    }
+
+    $scope.injectCounter = function() {
+
+        var startDateUTC = new Date('6/10/2016 19:00:00 UTC');
+        $scope.startDateLocal = js_yyyy_mm_dd_hh_mm_ss(startDateUTC);
+
+        // injection in HTML :
+        $("#counter").html("<div id='DateCountdown' data-date='"+$scope.startDateLocal+"' style='width: 100%;'></div>");
+
+        $("#DateCountdown").TimeCircles({
+            "animation": "smooth",
+            "bg_width": 1.2,
+            "fg_width": 0.1,
+            "circle_bg_color": "#60686F",
+            "time": {
+                "Days": {
+                    "text": "Jours",
+                    "color": "#FFCC66",
+                    "show": true
+                },
+                "Hours": {
+                    "text": "Heures",
+                    "color": "#99CCFF",
+                    "show": true
+                },
+                "Minutes": {
+                    "text": "Minutes",
+                    "color": "#BBFFBB",
+                    "show": true
+                },
+                "Seconds": {
+                    "text": "Secondes",
+                    "color": "#FF9999",
+                    "show": true
+                }
             }
-        }
-    });
+        });
+    }
 
 });
 
