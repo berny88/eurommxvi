70d8711d024df46228cd81c6985ce3cf5247cf1f
Bernard Bougeon <bernard.bougeon@gmail.com>
2016-06-08 18:43:53 +0200

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='euroXXXVI',
       version='1.0',
-      description='the most simple bet web site to enjoy with friends',
+      description='the most simple bet web site to enjoy with friends.',
       author='Bernard Bougeon',
       author_email='eurommxvi.foot@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
