6cd0476d5f0659a0a19f3df7b2420d25fdc4f6ee
Bernard Bougeon <bernard.bougeon@gmail.com>
2016-06-11 21:57:47 +0200

    test new welcome page

--- communities/static/communities.js
+++ communities/static/communities.js
@@ -24,6 +24,19 @@ euro2016App.controller('CommunitiesCtrl', ['$scope', '$routeParams', '$http', '$
             });
         }
 
+        $scope.getMyCommunities = function() {
+            hideAlerts();
+            $http.get('communities/apiv1.0/communities?'+getConnectedUser($window).user_id, {timeout: canceler.promise})
+            .success(function(data, status, headers, config) {
+                $scope.communities = data;
+                $('#spin').hide();
+            })
+            .error(function(data, status, headers, config) {
+                showAlertError("Erreur lors de la récupération de la liste des communautés ; erreur HTTP : " + status);
+                $('#spin').hide();
+            });
+        }
+
         $scope.getCommunity = function() {
             hideAlerts();
             $http.get('communities/apiv1.0/communities/' + $routeParams.com_id, {timeout: canceler.promise})
