8dfdfae9f512371a3d2e1fcb7721a01786735397
toto.squilaci <toto.squilaci>
2016-07-12 20:34:56 +0200

    Merge branch 'master' of ssh://euroxxxvi-typhontonus.rhcloud.com/~/git/euroxxxvi

--- .openshift/cron/minutely/store_history_ranking.sh
+++ .openshift/cron/minutely/store_history_ranking.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
+# Compét. finie, on commente les lignes ci-dessous
+
 # 18h00 en heure du serveur = minuit en France
-if [ `date +%H:%M` == "18:00" ]
-then
-    date >> ${OPENSHIFT_LOG_DIR}/store_history_ranking.log 2>&1
-    cd ${OPENSHIFT_REPO_DIR}
-    python stats/store_history_ranking.py >> ${OPENSHIFT_LOG_DIR}/store_history_ranking.log 2>&1
-fi
+#if [ `date +%H:%M` == "18:00" ]
+#then
+#    date >> ${OPENSHIFT_LOG_DIR}/store_history_ranking.log 2>&1
+#    cd ${OPENSHIFT_REPO_DIR}
+#    python stats/store_history_ranking.py >> ${OPENSHIFT_LOG_DIR}/store_history_ranking.log 2>&1
+#fi
